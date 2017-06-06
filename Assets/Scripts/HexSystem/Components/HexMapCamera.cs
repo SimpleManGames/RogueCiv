9ea8c69e295707ad98b38fcae4468c97a5f817f1
Riley <simplemangame@gmail.com>
2017-06-05 23:37:11 -0700

    Camera now moves with WASD and moves faster when zoomed out and vice-versa

--- Assets/Scripts/HexSystem/Components/HexMapCamera.cs
+++ Assets/Scripts/HexSystem/Components/HexMapCamera.cs
@@ -16,6 +16,11 @@ public class HexMapCamera : MonoBehaviour
     [SerializeField]
     private float swivelMaxZoom;
 
+    [SerializeField]
+    private float moveSpeedMinZoom;
+    [SerializeField]
+    private float moveSpeedMaxZoom;
+
     public void Awake()
     {
         swivel = transform.GetChild(0);
@@ -27,6 +32,12 @@ public class HexMapCamera : MonoBehaviour
         float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
         if (zoomDelta != 0f)
             AdjustZoom(zoomDelta);
+
+        float xDelta = Input.GetAxis("Horizontal");
+        float zDelta = Input.GetAxis("Vertical");
+
+        if (xDelta != 0f || zDelta != 0f)
+            AdjustPosition(xDelta, zDelta);
     }
 
     private void AdjustZoom(float delta)
@@ -38,5 +49,16 @@ public class HexMapCamera : MonoBehaviour
 
         float angle = Mathf.Lerp(swivelMinZoom, swivelMaxZoom, zoom);
         swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
+    }
+
+    private void AdjustPosition(float xDelta, float zDelta)
+    {
+        Vector3 direction = new Vector3(xDelta, 0f, zDelta).normalized;
+        float damping = Mathf.Max(Mathf.Abs(xDelta), Mathf.Abs(zDelta));
+        float distance = Mathf.Lerp(moveSpeedMinZoom, moveSpeedMaxZoom, zoom) * damping * Time.fixedDeltaTime;
+
+        Vector3 position = transform.position;
+        position += direction * distance;
+        transform.localPosition = position;
     }
 }
