61ea8945f3de0cb96701508bef7388b3c9a42f26
Riley <simplemangame@gmail.com>
2017-06-12 22:55:20 -0700

    Added Button Attribute to make running functions in the editor easier

---
+++ Assets/Attributes/ButtonAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+public enum ButtonMode
+{
+    AlwaysEnabled,
+    EnabledInPlayMode,
+    DisabledInPlayMode
+}
+/// <summary>
+/// Attribute to create a button in the inspector for calling the method it is attached to.
+/// The method must have no arguments.
+/// </summary>
+/// <example>
+/// [Button]
+/// public void MyMethod()
+/// {
+///     Debug.Log("Clicked!");
+/// }
+/// </example>
+[AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+public sealed class ButtonAttribute : Attribute
+{
+    public ButtonMode mode;
+    public ButtonAttribute(ButtonMode mode = ButtonMode.AlwaysEnabled)
+    {
+        this.mode = mode;
+    }
+}
