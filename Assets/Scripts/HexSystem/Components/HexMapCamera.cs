04d5818fbfd37ebf92fd6864025dae8d4ebefda9
Riley <simplemangame@gmail.com>
2017-06-12 22:59:05 -0700

    Fixed the camera speed by changing in to normal update

--- Assets/Scripts/HexSystem/Components/HexMapCamera.cs
+++ Assets/Scripts/HexSystem/Components/HexMapCamera.cs
@@ -46,7 +46,7 @@ public class HexMapCamera : MonoBehaviour
         AdjustRotation(0f);
     }
 
-    public void LateUpdate()
+    public void Update()
     {
         float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
         if (zoomDelta != 0f)
@@ -76,7 +76,7 @@ public class HexMapCamera : MonoBehaviour
 
     private void AdjustRotation(float delta)
     {
-        rotationAngle += delta * Time.fixedDeltaTime * rotationSpeed;
+        rotationAngle += delta * Time.deltaTime * rotationSpeed;
 
         if (rotationAngle < 0f)
             rotationAngle += 360f;
@@ -90,7 +90,7 @@ public class HexMapCamera : MonoBehaviour
     {
         Vector3 direction = transform.localRotation * new Vector3(xDelta, 0f, zDelta).normalized;
         float damping = Mathf.Max(Mathf.Abs(xDelta), Mathf.Abs(zDelta));
-        float distance = Mathf.Lerp(moveSpeedMinZoom, moveSpeedMaxZoom, zoom) * damping * Time.fixedDeltaTime;
+        float distance = Mathf.Lerp(moveSpeedMinZoom, moveSpeedMaxZoom, zoom) * damping * Time.deltaTime;
 
         Vector3 position = transform.position;
         position += direction * distance;
